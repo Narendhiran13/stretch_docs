d1044ab500380aa5714ad83d75861ba6b85c5b16
Aaron Edsinger <aedsinger@hello-robot.com>
2022-07-17 14:08:16 -0700

    ready to test mike on master

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #rm -rf ./site
-cd ../aedsinger.github.io/
-mkdocs gh-deploy --config-file ../stretch_docs_dev/mkdocs.yml --remote-branch master
+cd ../hello-robot.github.io/
+mkdocs gh-deploy --config-file ../stretch_docs/mkdocs.yml --remote-branch master
 
